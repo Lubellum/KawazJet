71be022a56cc9fa9d585afc8a5712a4d7158c3ad
Lubellum <90450493+Lubellum@users.noreply.github.com>
2026-07-18 11:30:16 +0900

    5.2 『Tiled Map Editor』を使ってマップを作成する

--- Resources/map/object.tsx
+++ Resources/map/object.tsx
@@ -1,29 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="object" tilewidth="24" tileheight="24">
+<tileset version="1.10" tiledversion="1.12.2" name="object" tilewidth="24" tileheight="24" tilecount="5" columns="5">
  <image source="object.png" width="120" height="24"/>
- <tile id="0">
-  <properties>
-   <property name="category" value="4"/>
-  </properties>
- </tile>
- <tile id="1">
-  <properties>
-   <property name="category" value="8"/>
-  </properties>
- </tile>
- <tile id="2">
-  <properties>
-   <property name="category" value="8"/>
-  </properties>
- </tile>
- <tile id="3">
-  <properties>
-   <property name="category" value="4"/>
-  </properties>
- </tile>
- <tile id="4">
-  <properties>
-   <property name="category" value="4"/>
-  </properties>
- </tile>
 </tileset>
